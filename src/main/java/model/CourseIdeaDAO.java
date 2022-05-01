bb911a23f911f10dd156e19b26672877b86be68d
Chris <suruceanu.cristian@gmail.com>
2022-05-01 12:38:35 +0300

    Initial commit

---
+++ src/main/java/model/CourseIdeaDAO.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+public interface CourseIdeaDAO {
+    boolean add(CourseIdea idea);
+
+    List<CourseIdea> findAll();
+
+    CourseIdea findBySlug(String slug);
+
+}
